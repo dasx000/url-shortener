6705e172e9d189a34e8569cb5d9cec0899ad29e5
DIKY ADISAPUTRA <68739579+dikyadisaputra@users.noreply.github.com>
2023-01-22 10:45:08 +0700

    return

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.post('/shortUrls', async (req, res) => {
   const shortUrl = await ShortUrl.findOne({ short: req.body.shorturl });
   console.log(shortUrl);
   const short = req.body.shortUrl;
-  if (short.length > 40) return;
+  if (short.length > 40) return res.redirect('/');
   if (shortUrl == null) {
     await ShortUrl.create({ full: req.body.fullUrl, short: short });
     return res.redirect('/');
